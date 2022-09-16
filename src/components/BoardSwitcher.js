16b17c9f32e44b5852bff97d9b63ee415bc41cba
Tianye Chen <78762926+tianye-chen@users.noreply.github.com>
2022-09-16 16:53:38 -0400

    Complete

--- src/components/BoardSwitcher.js
+++ src/components/BoardSwitcher.js
@@ -9,19 +9,27 @@ function Board(props) {
   return <div className={className}>{props.index + 1}</div>;
 }
 
-function BoardSwitcher(props) {
-  let boards = [];
-  for (let ii = 0; ii < props.numBoards; ii++) {
-    let isSelected = ii === 0;
-    boards.push(<Board index={ii} selected={isSelected} key={ii} />);
-  }
 
-  return (
-    <div>
-      <div className="boards">{boards}</div>
-      <button>Toggle</button>
-    </div>
-  );
+
+function BoardSwitcher(props) {
+    const [selected, setSelected] = React.useState(0);
+
+    function handleClick(e) {
+        setSelected((prev) => (prev === 4 ? 0 : prev + 1));
+    }
+
+    let boards = [];
+    for (let ii = 0; ii < props.numBoards; ii++) {
+        let isSelected = ii === selected;
+        boards.push(<Board index={ii} selected={isSelected} key={ii} />);
+    }
+
+    return (
+        <div>
+            <div className="boards">{boards}</div>
+            <button onClick={handleClick}>Toggle</button>
+        </div>
+    );
 }
 
 export default BoardSwitcher;
