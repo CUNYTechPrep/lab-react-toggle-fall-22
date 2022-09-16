72f74465d6165f5c7d6faee6692ecd9bf423bee8
Jaichyi Chen <jaichyichen@gmail.com>
2022-09-16 17:42:04 -0400

    initial commit

--- src/components/BoardSwitcher.js
+++ src/components/BoardSwitcher.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Board(props) {
   let className = "board";
@@ -10,16 +10,24 @@ function Board(props) {
 }
 
 function BoardSwitcher(props) {
+  const [count, setCount] = useState(0);
+  const eventHandler = (e) => {
+    if (count >= props.numBoards) {
+      return setCount(0);
+    } else {
+      return setCount(count + 1);
+    }
+  };
   let boards = [];
   for (let ii = 0; ii < props.numBoards; ii++) {
-    let isSelected = ii === 0;
+    let isSelected = ii === count;
     boards.push(<Board index={ii} selected={isSelected} key={ii} />);
   }
 
   return (
     <div>
       <div className="boards">{boards}</div>
-      <button>Toggle</button>
+      <button onClick={eventHandler}>Toggle</button>
     </div>
   );
 }
