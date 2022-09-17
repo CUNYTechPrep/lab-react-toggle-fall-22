e0047f87136e01ded1eca5097022cd1189980a8e
Norikaz <norikzhagui@gmail.com>
2022-09-17 12:47:24 -0400

    Implemented togglefunction

--- src/components/BoardSwitcher.js
+++ src/components/BoardSwitcher.js
@@ -1,4 +1,5 @@
-import React from "react";
+
+import React, { useState } from "react";
 
 function Board(props) {
   let className = "board";
@@ -16,12 +17,27 @@ function BoardSwitcher(props) {
     boards.push(<Board index={ii} selected={isSelected} key={ii} />);
   }
 
+
+  const [currSel,setCurSel] = useState(0);
+  const [refBoard,setBoard] = useState(boards);
+
+  const f = (e) => { 
+    boards = [];
+    setCurSel(currSel+1);
+    if(currSel >= props.numBoards-1) setCurSel(0);
+    for (let ii = 0; ii < props.numBoards; ii++) {
+      let isSelected = ii === currSel;
+      boards.push(<Board index={ii} selected={isSelected} key={ii} />);
+    }
+    setBoard(boards);
+  }
+
   return (
     <div>
-      <div className="boards">{boards}</div>
-      <button>Toggle</button>
+      <div className="boards">{refBoard}</div>
+      <button onClick={f}>Toggle</button>
     </div>
   );
-}
 
+  }
 export default BoardSwitcher;
