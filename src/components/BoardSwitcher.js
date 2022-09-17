6beea4ed375d3006b9ab717af5027fe6626555e8
chystarr <chynnastarr2@gmail.com>
2022-09-17 13:04:35 -0400

    Add state to track current selection and event handler for toggle button

--- src/components/BoardSwitcher.js
+++ src/components/BoardSwitcher.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Board(props) {
   let className = "board";
@@ -10,16 +10,27 @@ function Board(props) {
 }
 
 function BoardSwitcher(props) {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  function handleClick() {
+    console.log("The button has been clicked");
+    if (selectedIndex === 4) {
+      setSelectedIndex(0);
+    } else {
+      setSelectedIndex(selectedIndex + 1);
+    }
+  }
+
   let boards = [];
   for (let ii = 0; ii < props.numBoards; ii++) {
-    let isSelected = ii === 0;
+    let isSelected = ii === selectedIndex; // isSelected is true if current board has a certain index
     boards.push(<Board index={ii} selected={isSelected} key={ii} />);
   }
 
   return (
     <div>
       <div className="boards">{boards}</div>
-      <button>Toggle</button>
+      <button onClick={handleClick}>Toggle</button>
     </div>
   );
 }
