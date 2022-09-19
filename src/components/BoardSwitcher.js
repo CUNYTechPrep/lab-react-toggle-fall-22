2ca73175328f3a724750b2c617892542bfdbea4f
Alfred-Huang <arvin5852i@gmail.com>
2022-09-19 13:26:36 -0400

    Use hook to keep track the click button

--- src/components/BoardSwitcher.js
+++ src/components/BoardSwitcher.js
@@ -1,25 +1,26 @@
-import React from "react";
+import React,  { useState } from "react";
 
 function Board(props) {
   let className = "board";
   if (props.selected) {
     className += " selected";
   }
-
+  
   return <div className={className}>{props.index + 1}</div>;
 }
 
 function BoardSwitcher(props) {
+  const [count, setCount] = useState(0);
   let boards = [];
   for (let ii = 0; ii < props.numBoards; ii++) {
-    let isSelected = ii === 0;
+    let isSelected = ii === (count % props.numBoards);
     boards.push(<Board index={ii} selected={isSelected} key={ii} />);
   }
 
   return (
     <div>
       <div className="boards">{boards}</div>
-      <button>Toggle</button>
+      <button onClick={() => setCount(count + 1)}>Toggle</button>
     </div>
   );
 }
