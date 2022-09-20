968d9a67af2e881d186c0db608a2cdba2ac00997
VincenzoCammilleri2 <cammilleriv2@gmail.com>
2022-09-20 15:30:25 -0400

    added ability to move across numBoards and return to start when reach end

--- src/components/BoardSwitcher.js
+++ src/components/BoardSwitcher.js
@@ -12,20 +12,22 @@ function Board(props) {
 }
 
 function BoardSwitcher(props) {
+
+  const [counter, setCount] = useState(0);
   let boards = [];
   for (let ii = 0; ii < props.numboards; ii++) {
-    let count = ii === 0;
+    let count = ii === counter % props.numboards;
     boards.push(<Board index={ii} selected={count} key={ii} />);
   }
 
   
-  const [count, setCount] = useState(0);
+
 
   return (
     <div>
       <div className="boards">{boards}</div>
-      <button onClick={() => setCount(count + 1)}>Toggle</button>
-      <p>You clicked {count} time</p>
+      <button onClick={() => setCount(counter + 1)}>Toggle</button>
+      <p>You clicked {counter} time</p>
     </div>
     //click counter works
   );
