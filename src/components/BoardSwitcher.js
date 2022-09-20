5a37d654e997ea5bd9ef27166af649d75f6ee6db
RyEbrEad <rrvra0118@gmail.com>
2022-09-20 12:31:36 -0400

    init

--- src/components/BoardSwitcher.js
+++ src/components/BoardSwitcher.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Board(props) {
   let className = "board";
@@ -11,15 +11,21 @@ function Board(props) {
 
 function BoardSwitcher(props) {
   let boards = [];
+  const [numSwitcher, setSwitcher] = useState(0);
+  const handleClick = (event) =>{
+    setSwitcher(numSwitcher + 1);
+  };
+
   for (let ii = 0; ii < props.numBoards; ii++) {
-    let isSelected = ii === 0;
+    let isSelected = ii === numSwitcher % 5;
     boards.push(<Board index={ii} selected={isSelected} key={ii} />);
   }
 
   return (
     <div>
       <div className="boards">{boards}</div>
-      <button>Toggle</button>
+      <button onClick={handleClick}>
+        Toggle</button>
     </div>
   );
 }
