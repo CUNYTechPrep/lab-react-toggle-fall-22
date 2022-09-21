342308e30f1ee76d4ee6d5aceb75fcf7f81971e5
fafatim <farhana.fatima.lima@gmail.com>
2022-09-21 15:16:48 -0400

    Completed react toggle lab

--- src/components/BoardSwitcher.js
+++ src/components/BoardSwitcher.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Board(props) {
   let className = "board";
@@ -11,15 +11,23 @@ function Board(props) {
 
 function BoardSwitcher(props) {
   let boards = [];
+  const [selectedBoard, setBoardValue] = useState(0)
+    const handleClick =(event) => {
+      if(selectedBoard === props.numBoards-1){
+        setBoardValue(0);
+      }else{
+        setBoardValue(selectedBoard+1);
+      }
+    }
   for (let ii = 0; ii < props.numBoards; ii++) {
-    let isSelected = ii === 0;
+    let isSelected = ii === selectedBoard;
     boards.push(<Board index={ii} selected={isSelected} key={ii} />);
   }
 
   return (
     <div>
       <div className="boards">{boards}</div>
-      <button>Toggle</button>
+      <button onClick={handleClick}>Toggle</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import BoardSwitcher from "./components/BoardSwitcher";
 import "./index.css";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BoardSwitcher numBoards={3} />
+    <BoardSwitcher numBoards={5} />
   </React.StrictMode>
 );
