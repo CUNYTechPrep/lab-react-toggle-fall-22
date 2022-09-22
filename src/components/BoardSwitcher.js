b278db330abad16894da57b57c696bd3fe5db1e5
Kevin Docanto <kevin-teus@hotmail.com>
2022-09-22 18:26:32 -0400

    adding files

--- src/components/BoardSwitcher.js
+++ src/components/BoardSwitcher.js
@@ -1,25 +1,31 @@
-import React from "react";
+import React from 'react';
+import { useState } from 'react';
 
 function Board(props) {
-  let className = "board";
+  let className = 'board';
   if (props.selected) {
-    className += " selected";
+    className += ' selected';
   }
 
   return <div className={className}>{props.index + 1}</div>;
 }
 
 function BoardSwitcher(props) {
+  const [selectedItem, setItem] = useState(0);
+  const handleClick = () => {
+    setItem((selectedItem + 1) % props.numBoards);
+  };
+
   let boards = [];
   for (let ii = 0; ii < props.numBoards; ii++) {
-    let isSelected = ii === 0;
+    let isSelected = ii === selectedItem;
     boards.push(<Board index={ii} selected={isSelected} key={ii} />);
   }
 
   return (
     <div>
       <div className="boards">{boards}</div>
-      <button>Toggle</button>
+      <button onClick={handleClick}>Toggle</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import BoardSwitcher from "./components/BoardSwitcher";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BoardSwitcher from './components/BoardSwitcher';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BoardSwitcher numBoards={3} />
+    <BoardSwitcher numBoards={5} />
   </React.StrictMode>
 );
