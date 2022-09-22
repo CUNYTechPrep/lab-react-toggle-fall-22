d00f9f501b3eeb5781a8eca8158823379839db72
fabio-pecora <fabiopecora01@gmail.com>
2022-09-21 23:01:54 -0400

    wednesday-section-fabio-pecora

--- src/components/BoardSwitcher.js
+++ src/components/BoardSwitcher.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Board(props) {
+  
+
   let className = "board";
   if (props.selected) {
     className += " selected";
@@ -9,17 +11,22 @@ function Board(props) {
   return <div className={className}>{props.index + 1}</div>;
 }
 
+
 function BoardSwitcher(props) {
+
+  let [boxSelected, setBoxSelected] = useState(0);
+
   let boards = [];
   for (let ii = 0; ii < props.numBoards; ii++) {
-    let isSelected = ii === 0;
+    let isSelected = ii === boxSelected;
     boards.push(<Board index={ii} selected={isSelected} key={ii} />);
   }
 
   return (
     <div>
       <div className="boards">{boards}</div>
-      <button>Toggle</button>
+      <button onClick={() => setBoxSelected((boxSelected + 1) % 5)}>Toggle</button>
+      <p> Current selection: {}</p>
     </div>
   );
 }
