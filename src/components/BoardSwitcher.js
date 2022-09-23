3c67c9b0767c545efc194c69e9821f080c2dc89b
Alexia Blake <alexia.blake@lc.cuny.edu>
2022-09-23 01:17:10 -0400

    Completed

--- src/components/BoardSwitcher.js
+++ src/components/BoardSwitcher.js
@@ -1,25 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Board(props) {
   let className = "board";
   if (props.selected) {
-    className += " selected";
+  className += " selected";
   }
 
   return <div className={className}>{props.index + 1}</div>;
 }
 
 function BoardSwitcher(props) {
+const[clickedIndex, setClicked] = useState(0);
+
+const handleClick = (event) =>{
+if(clickedIndex <= props.numBoards){
+  setClicked(clickedIndex + 1);
+}
+else{
+  setClicked(0);
+}
+};
   let boards = [];
   for (let ii = 0; ii < props.numBoards; ii++) {
-    let isSelected = ii === 0;
+    let isSelected = ii === clickedIndex;
     boards.push(<Board index={ii} selected={isSelected} key={ii} />);
   }
-
+  
   return (
     <div>
       <div className="boards">{boards}</div>
-      <button>Toggle</button>
+      <button onClick={handleClick}>Toggle</button>
     </div>
   );
 }
