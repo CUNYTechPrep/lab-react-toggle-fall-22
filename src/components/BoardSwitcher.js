455c77ddeca45855944ec537c45d1e94b817418a
mmim14 <mominamim21@gmail.com>
2022-09-27 16:48:25 -0400

    Finished the tasks
    - Board number is updated from 3 to 5
    - a variable with state to track the state has been added and an event handler to utilize it as well
    - Toggle button calls handleToggle when clicked

--- src/components/BoardSwitcher.js
+++ src/components/BoardSwitcher.js
@@ -1,4 +1,5 @@
-import React from "react";
+//import React from "react";
+import React, { useState } from "react";
 
 function Board(props) {
   let className = "board";
@@ -9,17 +10,35 @@ function Board(props) {
   return <div className={className}>{props.index + 1}</div>;
 }
 
+
 function BoardSwitcher(props) {
   let boards = [];
+
+  const [boxSelected, setBoxSelected] = useState(0);
+  const handleToggle = (event) =>{
+    setBoxSelected((prevBoxSelected) => {
+      let newBoxSelected = prevBoxSelected+1;
+      if (newBoxSelected === boards.length) newBoxSelected = newBoxSelected % boards.length;
+      return newBoxSelected;
+    });
+  }
+  /*
+    how is the event handler connected with the variables with states? 
+    what is allowed insie the function call of set<UseStateVariable>? 
+    last time we did simple arithmetic this time we have an arrow function with
+        a parameter 
+  */
+  
   for (let ii = 0; ii < props.numBoards; ii++) {
-    let isSelected = ii === 0;
+    let isSelected = ii === boxSelected;
     boards.push(<Board index={ii} selected={isSelected} key={ii} />);
   }
 
   return (
     <div>
       <div className="boards">{boards}</div>
-      <button>Toggle</button>
+      <button onClick={handleToggle}>Toggle</button>
+      <handleToggle/>
     </div>
   );
 }
