67e40a2b29be57e7152b155361b8dd7435e724bb
ravenclaw1999 <702schowdhury@gmail.com>
2022-10-07 16:00:52 -0400

    working toggle

--- src/components/BoardSwitcher.js
+++ src/components/BoardSwitcher.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 
 function Board(props) {
   let className = "board";
@@ -10,16 +10,23 @@ function Board(props) {
 }
 
 function BoardSwitcher(props) {
+  const [selectedBoard, setSelectedBoard] = useState(0)
+
+  const handleClick = () => { 
+    setSelectedBoard((selectedBoard + 1) % props.numBoards);
+    console.log("clicked the toggle button");
+  };
+
   let boards = [];
   for (let ii = 0; ii < props.numBoards; ii++) {
-    let isSelected = ii === 0;
+    let isSelected = ii === selectedBoard;
     boards.push(<Board index={ii} selected={isSelected} key={ii} />);
   }
 
   return (
     <div>
       <div className="boards">{boards}</div>
-      <button>Toggle</button>
+      <button onClick={handleClick}>Toggle</button>
     </div>
   );
 }
